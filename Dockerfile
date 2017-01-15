7081a47fa09990479bfc782c562cd6d413b76036
Your Name <you@example.com>
2017-01-15 11:46:11 +0800

    add SET FOR REDIS

---
+++ Dockerfile
@@ -0,0 +1,30 @@
+FROM python:2.7.11
+
+ENV TZ "Asia/Shanghai"
+
+RUN pip install -i https://pypi.douban.com/simple/ rethinkpool 
+RUN pip install -i https://pypi.douban.com/simple/ rethinkdb 
+RUN pip install -i https://pypi.douban.com/simple/ requests 
+RUN pip install -i https://pypi.douban.com/simple/ redis
+
+
+
+# Work in app dir by default.
+WORKDIR /app
+
+COPY . /app
+
+
+ENV ANDROID_HOME /app/adb/
+ENV PATH /app:$PATH
+
+# Export default app port, not enough for all processes but it should do
+# for now.
+USER root
+ENTRYPOINT ["python","MainTask.py"] 
+CMD [""]  
+
+
+
+
+
--- zcache/__init__.py
+++ zcache/__init__.py
@@ -11,11 +11,15 @@ class _cache:
     def __init__(self):
         self.pool = redis.ConnectionPool(host=const.REDIS_SERVER, port=6379, db=0)
 
-    def push(self, key, value, timeout=300):
-        r = redis.Redis(connection_pool=self.pool)
 
-    def pop(self, key):
+
+    def addSet(self, key, value, timeout=300):
         r = redis.Redis(connection_pool=self.pool)
+        r.sadd(key, value)
+
+    def popSet(self, key):
+        r = redis.Redis(connection_pool=self.pool)
+        return r.spop(key)
 
     def set(self, key, value, timeout=300):
         r = redis.Redis(connection_pool=self.pool)
@@ -32,3 +36,8 @@ class _cache:
 cache = _cache()
 
 
+cache.addSet('aaaaa', 'a')
+cache.addSet('aaaaa', 'b')
+cache.addSet('aaaaa', 'c')
+cache.set('abc', 'c')
+print cache.popSet('aaaaa')
