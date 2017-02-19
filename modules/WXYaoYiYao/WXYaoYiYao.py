45a73d8c2f678d642f054f3e7584bc832552ec6c
Your Name <you@example.com>
2017-02-19 12:58:37 +0800

    update

--- modules/WXYaoYiYao/WXYaoYiYao.py
+++ modules/WXYaoYiYao/WXYaoYiYao.py
@@ -122,7 +122,7 @@ class WXYaoYiYao:
                 d(textContains='相距').click()
                 d(text='打招呼').click()
                 d(className='android.widget.EditText').click()
-                z.input(Material)
+                z.input('好想给你发红包')     #Material
                 d(text='发送').click()
                 t = t+1
                 d(description='返回').click()
